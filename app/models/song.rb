7f481881c8ad7f11bd01bf58af72777522973362
Matt Farmer <mrfarmer777@gmail.com>
2018-07-16 01:08:53 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,20 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist=Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_id=(id)
+    self.genre=Genre.find(id)
+  end
+
+  def genre_id
+    self.genre ? self.genre.id : nil
+  end
+end
